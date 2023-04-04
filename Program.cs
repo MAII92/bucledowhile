b3a6cd356cb676c0733f06562d305515515a725a
maguilarm <maguilarm@WSAMZN-C7PDID55>
2023-04-04 17:57:43 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+using System;
+
+namespace bucledowhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int z = 10;
+
+            do {
+                Console.WriteLine("impresion!");
+
+                z++;
+
+            }    while(z<  15) ;
+        }
+    }
+}
